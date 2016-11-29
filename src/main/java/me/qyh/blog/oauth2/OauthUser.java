e320811f4a2e26b85ae81701c2c62a6bcc6b0f34
mhlx <admin@qyh.me>
2016-11-29 20:10:36 +0800

    去掉了OauthUser的邮件属性

--- src/main/java/me/qyh/blog/oauth2/OauthUser.java
+++ src/main/java/me/qyh/blog/oauth2/OauthUser.java
@@ -25,7 +25,6 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import me.qyh.blog.entity.BaseEntity;
 import me.qyh.blog.input.JsonHtmlXssSerializer;
 import me.qyh.blog.message.Message;
-import me.qyh.util.Validators;
 
 /**
  * 这里没有保存凭证<br>
@@ -52,7 +51,6 @@ public class OauthUser extends BaseEntity {
 	private String serverId;
 	private String serverName;
 	private Boolean admin;
-	private String email;
 
 	/**
 	 * oauth用户状态
@@ -162,18 +160,6 @@ public class OauthUser extends BaseEntity {
 
 	public void setServerName(String serverName) {
 		this.serverName = serverName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public boolean hasEmail() {
-		return !Validators.isEmptyOrNull(email, true);
 	}
 
 	@Override
