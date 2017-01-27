b76139c2a978a7dba15cd98cd91f66e9775dd149
mhlx <admin@qyh.me>
2017-01-27 19:05:43 +0800

    update sql

--- blog.init.sql
+++ blog.init.sql
@@ -86,6 +86,7 @@ CREATE TABLE IF NOT EXISTS `blog_fragment_user` (
   `fragment_create_date` datetime NOT NULL,
   `space_id` int(11) DEFAULT NULL,
   `is_global` tinyint(1) NOT NULL DEFAULT '0',
+  `is_callable` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ;
 
